5d39d58a7983a98286ac24143570bb81dbf8bd5b
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-07-23 15:38:43 -0400

    feat: create global function for adding elements

--- logica_projeto.py/estruturas.py
+++ logica_projeto.py/estruturas.py
@@ -1,13 +1,11 @@
 # estrutura dos dados
-from menu import menu_numerado, limpar_tela
+from menu import menu_numerado, pilhas_ou_filas, limpar_tela
 
-opcao, pilha_ou_fila = menu_numerado()
 
-def trabalhar_pilhas(lista_pilhas):
-    pass
 
 def adicionar_elemento():
-
+    """ função global para adicionar elementos 
+    independente do tipo de lista """
 
     while True:
         entrada = input('Qual elemento deseja adicionar: ')
@@ -23,24 +21,28 @@ def adicionar_elemento():
 lista_pilhas = []
 lista_filas = []
 
-while True:
-    limpar_tela()
-    menu_numerado()
-    match opcao:
-        case 1:
-            elemento = adicionar_elemento()
+def rodar_programa():
+    while True:
+        limpar_tela()
+        opcao = menu_numerado()
+        match opcao:
+            case 1:
+                    
+                pilha_ou_fila = pilhas_ou_filas()
+                elemento = adicionar_elemento()
 
-            if pilha_ou_fila == 1:
-                lista_pilhas.append(elemento)
-            else:
-                lista_filas.append(elemento)
+                if pilha_ou_fila == 1:
+                    lista_pilhas.append(elemento)
+                else:
+                    lista_filas.append(elemento)
 
-        case 2 :
-            pass
+            case 2 :
+                pass
 
-        case 3:
-            pass
+            case 3:
+                pass
 
-        case 4:
-            print('\nprograma encerrado.\n')
-            break
+            case 4:
+                print('\nprograma encerrado.\n')
+                break
+
