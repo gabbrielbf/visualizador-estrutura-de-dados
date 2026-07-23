c76cc2b6221a2aa3be872985e6ff9295a1fb3cae
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-07-23 12:05:27 -0400

    feat: ask user whether they want to work with stacks or queues

--- logica_projeto.py/menu.py
+++ logica_projeto.py/menu.py
@@ -1,3 +1,6 @@
+def ler_opcao_numerica():
+    pass
+
 def menu_numerado():
 
     opcoes = ['Adicionar elemento', 'Remover elemento', 'Ver estado atual', 'Encerrar'] # <- lista criada apenas para exibição dinâmica
@@ -8,6 +11,7 @@ def menu_numerado():
     print('-'*30)                   #  poupar espaço e não ter que lotar o inicio da função de 'prints'
 
     while True:
+        
         try:
             opcao = int(input('O que deseja fazer -> '))
         except ValueError:  # <- esse bloco é responsável por conferir uma entrada numérica
@@ -21,6 +25,27 @@ def menu_numerado():
         else:
             break
 
-    return opcao
+    opcoes = ['Pilhas', 'Filas']
+
+    print('\nCom o que deseja trabalhar:')
+    for indice, lista in enumerate(opcoes, start=1):
+        print(f'{indice} - {lista}')
+
+    while True:
+
+        try:
+            pilha_ou_fila = int(input('Selecione uma opção -> '))
+        except ValueError: 
+            print('[ERRO] valor inválido\n')
+            continue
+
+        if (pilha_ou_fila < 1 or
+            pilha_ou_fila > 2):
+            print('[ERRO] opção não encontrada\n')
+            continue
+        else:
+            break
+
+    return opcao, pilha_ou_fila
 
 menu_numerado()
